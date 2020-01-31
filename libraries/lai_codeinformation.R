3d97e89c26a6acbc25d131c98db283de056e60c5
Laurence Lin <laurence.lin@icloud.com>
2020-01-31 16:09:21 -0500

    cusomtized lai code

--- libraries/lai_codeinformation.R
+++ libraries/lai_codeinformation.R
@@ -1,5 +1,6 @@
 arg=commandArgs(T)
 
+w = read.csv(arg[1])
 output = data.frame(codeID = as.numeric( gsub('[a-z]','',colnames(w)[-1:-2]) ))
 output$vegID1 = rep(0,dim(output)[1])
 write("name",arg[2],ncolumns=1,append=F)
